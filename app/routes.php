66a29a89a76af3475bd20745f62087a655a5f0d9
Ivan <ivan@linuxmint.linuxmint-domain>
2014-04-11 19:39:45 +0700

    Benerin Logout

--- app/routes.php
+++ app/routes.php
@@ -38,6 +38,6 @@ Route::get('/humas',array('as'=>'humas',function()
 {
 	return View::make('humas',array('title' => 'Humas'));
 }));
-Route::get('logout','LoginLogoutManager@logout');
+Route::get('logout',array('as'=>'logout','uses'=>'LoginLogoutManager@logout'));
 
 
