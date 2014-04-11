3f8f42b85e804b1007fac939dae6bf778519e4cc
gedehermawan <gedehermawan2606@gmail.com>
2014-04-11 22:49:54 +0700

    tabel udah jadi
    foreign key udah komplit semua

---
+++ app/database/migrations/2014_04_11_153612_add_foreign_key_id_ruangan_to_forms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeyIdRuanganToFormsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('forms', function(Blueprint $table) {
+			$table->integer('id_ruangan');
+			$table->foreign('id_ruangan')->references('id')->on('ruangan');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('forms', function(Blueprint $table) {
+			$table->dropColumn('id_ruangan');
+		});
+	}
+
+}
