0d1033ae1d4520c411d94e0f4cff5fcf9656efc2
Ibrahim Nurandita Isbandiputra <ibrahimker@gmail.com>
2014-04-14 14:02:32 +0700

    Tambah deskripsi.blade.php
    Signed-off-by: Ibrahim Nurandita Isbandiputra <ibrahimker@gmail.com>

---
+++ app/views/deskripsi.blade.php
@@ -0,0 +1,40 @@
+@extends('layout')
+@section('content')
+<!-- table starts here -->
+<div id="table">
+	<table class="ui table segment">
+		<thead>
+	    	<tr>
+		    	<th>Name</th>
+		    	<th>Lantai</th>
+		    	<th>Kapasitas</th>
+			    <th>Status</th>
+			    <th>Fasilitas</th>
+	  		</tr>
+		</thead>
+	  	<tbody>
+		    <tr>
+		      	<td>John</td>
+		      	<td>Approved</td>
+		      	<td>None</td>
+		      	<td>None</td>
+		      	<td>None</td>
+		    </tr>
+		    <tr>
+		      	<td>John</td>
+		      	<td>Approved</td>
+		      	<td>None</td>
+		      	<td>None</td>
+		      	<td>None</td>
+		    </tr>
+		    <tr>
+		      	<td>John</td>
+		      	<td>Approved</td>
+		      	<td>None</td>
+		      	<td>None</td>
+		    </tr>
+	  	</tbody>
+	</table>
+</div>
+
+@stop
