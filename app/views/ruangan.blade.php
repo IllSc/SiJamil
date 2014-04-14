ab93e1ea8b5e49c05f2b799a4db3814c5d510822
Ibrahim Nurandita Isbandiputra <ibrahimker@gmail.com>
2014-04-14 15:18:03 +0700

    Bisa Filter
    Signed-off-by: Ibrahim Nurandita Isbandiputra <ibrahimker@gmail.com>

--- app/views/ruangan.blade.php
+++ app/views/ruangan.blade.php
@@ -21,44 +21,41 @@
 		<p>tidak ada ruangan pada database</p>
 		@else
 		<!-- table starts here -->
-		<div id="table">
-			<table class="ui table segment">
-				<thead>
-			    	<tr>
-				    	<th>Nama Ruangan</th>
-				    	<th>Lantai</th>
-				    	<th>Kapasitas</th>
-					    <th>Status</th>
-					    <th>Pinjam</th>
-					    <th>Lihat Deskripsi Ruangan</th>
+		<table class="ui sortable table segment">
+			<thead>
+		    	<tr>
+			    	<th>Nama Ruangan</th>
+			    	<th>Lantai</th>
+			    	<th>Kapasitas</th>
+				    <th>Status</th>
+				    <th>Pinjam</th>
+				    <th>Lihat Deskripsi Ruangan</th>
 
-			  		</tr>
-				</thead>
-			  	<tbody>
-					@foreach($ruangan as $r)
-						<tr>
-							<td>{{$r->nomor_ruangan}}</td>
-							<td>{{$r->lantai}}</td>
-							<td>{{$r->kapasitas}}</td>
-							<td>Palsu</td>
-							<td>
-								<div class="circular ui icon button">
-									<a href="{{action('RuanganManager@pinjam',$r->id)}}">
-										<i class="ok circle icon"></i>
-									</a>
-								</div>
-							</td>
-							<td>
-								<div class="circular ui icon button">
-									<a href="{{action('RuanganManager@show',2)}}" class="">
-									<i class="ok circle icon"></i></a>
-								</div>
-							</td>
-						</tr>
-				    @endforeach
-			  	</tbody>
-			</table>
-			@endif
-		</div>
-
+		  		</tr>
+			</thead>
+		  	<tbody>
+				@foreach($ruangan as $r)
+					<tr>
+						<td>{{$r->nomor_ruangan}}</td>
+						<td>{{$r->lantai}}</td>
+						<td>{{$r->kapasitas}}</td>
+						<td>Palsu</td>
+						<td>
+							<div class="circular ui icon button">
+								<a href="{{action('RuanganManager@pinjam',$r->id)}}">
+									<i class="ok circle icon"></i>
+								</a>
+							</div>
+						</td>
+						<td>
+							<div class="circular ui icon button">
+								<a href="{{action('RuanganManager@show',2)}}" class="">
+								<i class="ok circle icon"></i></a>
+							</div>
+						</td>
+					</tr>
+			    @endforeach
+		  	</tbody>
+		</table>
+		@endif
 @stop
